37e1221ac924203be27d33f7cf87e5897267fe0f
Apoorva-14 <apoorvasharma141195@gmail.com>
2020-05-28 02:15:21 +0530

    Better version

--- todowoo/urls.py
+++ todowoo/urls.py
@@ -26,7 +26,7 @@ urlpatterns = [
 
     # Todos
     path('current/', views.currenttodos, name="currenttodos"),
-    path('home/', views.home, name="home"),
+    path('', views.home, name="home"),
     path('create/', views.createtodo, name="createtodo"),
     path('todo/<int:todo_pk>', views.viewtodo, name="viewtodo"),
     path('todo/<int:todo_pk>/complete', views.completetodo, name="completetodo"),
